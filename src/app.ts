96764a6328eaf857fc96ae4ddd5f548d82b61224
Jose Moreira Chacon <josemoreirachacon06@gmail.com>
2023-07-05 14:11:27 -0300

    adicionado cors

--- src/app.ts
+++ src/app.ts
@@ -59,6 +59,13 @@ class App {
 
   private initializeMiddlewares() {
     this.app.use(bodyParser.json());
+    this.app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      next();
+    });
   }
 
   private initializeControllers() {
